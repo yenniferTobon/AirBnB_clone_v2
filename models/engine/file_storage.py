9cfabd6bfc328a15702d0a3c783c61fadda9e07b
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-18 05:09:42 +0000

    Commit # 05: [Upload] file_storage.py with delete and all methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls:
+            a_dict = {}
+            for key, value in self.__objects.items():
+                if (cls == type(value)):
+                    a_dict[key] = value
+            return a_dict
+        else:
+            return (self.__objects)
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside
+        """
+        if obj:
+            k = obj.__class__.__name__ + '.' + obj.id
+            if k in self.__objects:
+                del self.__objects[k]
