7d6a450047f278fb1e5344f82604c51bd94ae5f7
yenniferTobon <ymtobony@unal.edu.co>
2019-12-19 14:27:50 -0500

    task 8

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """This is the city class"""
 from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy import Column, String, DateTime, ForeignKey, relationship
 from sqlalchemy.orm import relationship
 
 
@@ -15,3 +15,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all,delete")
--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, relationship
 
 
 class User(BaseModel, Base):
@@ -17,3 +17,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all,delete")
