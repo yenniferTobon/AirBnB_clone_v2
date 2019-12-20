cfdc85b16a594c00620e649bea324d7c81d7bfb8
yenniferTobon <ymtobony@unal.edu.co>
2019-12-19 22:57:03 -0500

    amenity.py correction HBNB_TYPE_STORAGE

--- models/amenity.py
+++ models/amenity.py
@@ -10,7 +10,4 @@ class Amenity(BaseModel, Base):
         name: input name
     """
     __tablename__ = 'amenities'
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
