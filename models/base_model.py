83644c24934c29f4018895a439705dbd7371dddb
yenniferTobon <ymtobony@unal.edu.co>
2020-01-22 08:42:39 -0500

    base_model.py control the error by isoformat()

--- models/base_model.py
+++ models/base_model.py
@@ -70,8 +70,10 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-        if '_sa_instance_state' in my_dict.keys():
+        try:
             del my_dict["_sa_instance_state"]
+        except KeyError:
+            pass
         return my_dict
 
     def delete(self):
