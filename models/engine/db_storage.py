3801524dd78aa21c9fb5c82263c46fad91cc5be9
yenniferTobon <ymtobony@unal.edu.co>
2020-01-22 08:01:10 -0500

    db_storage.py-task 7,  call close() method to close this session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,3 +68,10 @@ class DBStorage():
         factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(factory)
         self.__session = Session()
+
+    def close(self):
+        """Close this Session,
+           This clears all items and ends any
+           transaction in progress
+        """
+        self.__session.close()
