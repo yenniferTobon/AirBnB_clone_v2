adba3ce747c527bf703411fa6a16587b0ddabaa2
yenniferTobon <ymtobony@unal.edu.co>
2020-01-22 23:26:06 -0500

    8-cities_by_states.py /cities_by_states: display a HTML page: (inside the tag BODY)

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+from models import storage
+from models import state
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(error):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """url: 0.0.0.0:5000/states_list: display a HTML page: """
+    list_states = storage.all("State")
+    list_cities = storage.all("City")
+    return render_template('8-cities_by_states.html', list_states=list_states, list_cities=list_cities)
+
+
+"""web application must be listening on 0.0.0.0, port 5000"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
